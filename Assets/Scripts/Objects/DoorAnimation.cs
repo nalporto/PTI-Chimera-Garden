2cfcf3cab203c3324a31f3d183d2363f7dd5c3eb
nalporto <nalportoalt@gmail.com>
2025-09-08 08:08:35 -0300

    Door PlaceHolder

--- Assets/Scripts/Objects/DoorAnimation.cs
+++ Assets/Scripts/Objects/DoorAnimation.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class DoorAnimation : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Animator doorAnimator; // Reference to the Animator component
+    public float interactionDistance = 3f; // Distance within which the player can interact
+    public GameObject player; // Reference to the player object (assignable in the Inspector)
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (player != null && Vector3.Distance(player.transform.position, transform.position) <= interactionDistance)
+        {
+            doorAnimator.SetBool("IsOpened", true); // Keep the door open while the player is near
+        }
+        else
+        {
+            doorAnimator.SetBool("IsOpened", false); // Close the door when the player moves away
+        }
     }
 }
